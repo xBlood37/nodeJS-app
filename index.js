d13ffb9ef840804d19007e1707cc3d9a2a34c0e6
xBlood37 <123d.123b@mail.ru>
2022-12-16 16:46:17 +0300

    package json edit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.end("Hello, world!");
+});
+
 app.listen(3000, () => {
   console.log("listening on 3000");
 });
